99a8b2bdf158652924bdeaaa336c9690cdc15eb9
Oromion <caznaranl@uni.pe>
2022-06-15 15:53:05 -0500

    Update

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@
 # out of source builds
 cmake -S ${GITPOD_REPO_ROOT}/flow-test-dumux -B ${GITPOD_REPO_ROOT}/build
 cmake --build ${GITPOD_REPO_ROOT}/build
-# CASES=(tracer_transport hello_world freeflow cavity shallow_water rotation infiltration porenetwork) # ${CASES[@]}
-tar -c -I 'zstd -19 -T0' -f ${GITPOD_REPO_ROOT}/build_$(date -u +"%Y-%m-%d-%H-%M-%S" --date='5 hours ago').tar.zst ${GITPOD_REPO_ROOT}/build/{tracer_transport,hello_world,freeflow,cavity,shallow_water,rotation,infiltration,porenetwork}
+# CASES=(tracer_transport hello_world freeflow cavity shallow_water rotation infiltration porenetwork bio) # ${CASES[@]}
+tar -c -I 'zstd -19 -T0' -f ${GITPOD_REPO_ROOT}/build_$(date -u +"%Y-%m-%d-%H-%M-%S" --date='5 hours ago').tar.zst ${GITPOD_REPO_ROOT}/build/{tracer_transport,hello_world,freeflow,cavity,shallow_water,rotation,infiltration,porenetwork,bio}
