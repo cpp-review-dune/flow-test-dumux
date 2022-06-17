5ec32d1fab8db5da815e977c75dd1896398fa34f
Carlos Aznarán Laos <caznaranl@uni.pe>
2022-06-17 12:47:34 -0500

    Add root_soil

---
+++ flow-test-dumux/root_soil/root_soil.cc
@@ -0,0 +1,229 @@
+// -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set et ts=4 sw=4 sts=4:
+/*****************************************************************************
+ *   See the file COPYING for full copying permissions.                      *
+ *                                                                           *
+ *   This program is free software: you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation, either version 3 of the License, or       *
+ *   (at your option) any later version.                                     *
+ *                                                                           *
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the            *
+ *   GNU General Public License for more details.                            *
+ *                                                                           *
+ *   You should have received a copy of the GNU General Public License       *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ *****************************************************************************/
+/*!
+ * \file
+ * \ingroup EmbeddedTests
+ * \brief Test for the 1d-3d embedded mixed-dimension model coupling two
+ *        one-phase porous medium flow problems.
+ */
+
+#include <config.h>
+
+#include <iostream>
+#include <fstream>
+
+#include <dune/common/parallel/mpihelper.hh>
+#include <dune/common/timer.hh>
+
+#include <dumux/common/initialize.hh>
+#include <dumux/common/properties.hh>
+#include <dumux/common/parameters.hh>
+#include <dumux/common/dumuxmessage.hh>
+#include <dumux/linear/seqsolverbackend.hh>
+#include <dumux/assembly/fvassembler.hh>
+#include <dumux/io/vtkoutputmodule.hh>
+#include <dumux/io/grid/gridmanager_yasp.hh>
+#include <dumux/io/grid/gridmanager_foam.hh>
+#include <dumux/io/grid/gridmanager_alu.hh>
+
+#include <dumux/multidomain/traits.hh>
+#include <dumux/multidomain/fvassembler.hh>
+#include <dumux/multidomain/newtonsolver.hh>
+
+#include "properties.hh"
+
+template<class CouplingManager, class Writer>
+void addElementMarkerForProjection(const CouplingManager& couplingManager, Writer& w)
+{
+    if constexpr(CouplingManager::couplingMode == Dumux::Embedded1d3dCouplingMode::projection)
+        w.addField(couplingManager.bulkElementMarker(), "element marker");
+}
+
+int main(int argc, char** argv)
+{
+    using namespace Dumux;
+
+    // maybe initialize MPI and/or multithreading backend
+    Dumux::initialize(argc, argv);
+    const auto& mpiHelper = Dune::MPIHelper::instance();
+
+    // print dumux start message
+    if (mpiHelper.rank() == 0)
+        DumuxMessage::print(/*firstCall=*/true);
+
+    // parse command line arguments and input file
+    Parameters::init(argc, argv);
+
+    // Define the sub problem type tags
+    using BulkTypeTag = Properties::TTag::SOILTYPETAG;
+    using LowDimTypeTag = Properties::TTag::Root;
+
+    // try to create a grid (from the given grid file or the input file)
+    // for both sub-domains
+    using BulkGridManager = Dumux::GridManager<GetPropType<BulkTypeTag, Properties::Grid>>;
+    BulkGridManager bulkGridManager;
+    bulkGridManager.init("Soil"); // pass parameter group
+
+    using LowDimGridManager = Dumux::GridManager<GetPropType<LowDimTypeTag, Properties::Grid>>;
+    LowDimGridManager lowDimGridManager;
+    lowDimGridManager.init("Root"); // pass parameter group
+
+    ////////////////////////////////////////////////////////////
+    // run instationary non-linear problem on this grid
+    ////////////////////////////////////////////////////////////
+
+    // we compute on the leaf grid view
+    const auto& bulkGridView = bulkGridManager.grid().leafGridView();
+    const auto& lowDimGridView = lowDimGridManager.grid().leafGridView();
+
+    // create the finite volume grid geometry
+    using BulkFVGridGeometry = GetPropType<BulkTypeTag, Properties::GridGeometry>;
+    auto bulkFvGridGeometry = std::make_shared<BulkFVGridGeometry>(bulkGridView);
+    using LowDimFVGridGeometry = GetPropType<LowDimTypeTag, Properties::GridGeometry>;
+    auto lowDimFvGridGeometry = std::make_shared<LowDimFVGridGeometry>(lowDimGridView);
+
+    // the mixed dimension type traits
+    using Traits = MultiDomainTraits<BulkTypeTag, LowDimTypeTag>;
+    constexpr auto bulkIdx = Traits::template SubDomain<0>::Index();
+    constexpr auto lowDimIdx = Traits::template SubDomain<1>::Index();
+
+    // the coupling manager
+    using CouplingManager = GetPropType<BulkTypeTag, Properties::CouplingManager>;
+    auto couplingManager = std::make_shared<CouplingManager>(bulkFvGridGeometry, lowDimFvGridGeometry);
+
+    // the problem (initial and boundary conditions)
+    using BulkProblem = GetPropType<BulkTypeTag, Properties::Problem>;
+    auto bulkProblem = std::make_shared<BulkProblem>(bulkFvGridGeometry, couplingManager);
+
+    // the low dim spatial parameters
+    using LowDimSpatialParams = GetPropType<LowDimTypeTag, Properties::SpatialParams>;
+    auto lowDimSpatialParams = std::make_shared<LowDimSpatialParams>(lowDimFvGridGeometry, lowDimGridManager.getGridData());
+
+    // the low dim problem (initial and boundary conditions)
+    using LowDimProblem = GetPropType<LowDimTypeTag, Properties::Problem>;
+    auto lowDimProblem = std::make_shared<LowDimProblem>(lowDimFvGridGeometry, lowDimSpatialParams, couplingManager);
+
+    // the solution vector
+    Traits::SolutionVector sol;
+    sol[bulkIdx].resize(bulkFvGridGeometry->numDofs());
+    sol[lowDimIdx].resize(lowDimFvGridGeometry->numDofs());
+    bulkProblem->applyInitialSolution(sol[bulkIdx]);
+    lowDimProblem->applyInitialSolution(sol[lowDimIdx]);
+    auto oldSol = sol;
+
+    couplingManager->init(bulkProblem, lowDimProblem, sol);
+    bulkProblem->computePointSourceMap();
+    lowDimProblem->computePointSourceMap();
+
+    // the grid variables
+    using BulkGridVariables = GetPropType<BulkTypeTag, Properties::GridVariables>;
+    auto bulkGridVariables = std::make_shared<BulkGridVariables>(bulkProblem, bulkFvGridGeometry);
+    bulkGridVariables->init(sol[bulkIdx]);
+    using LowDimGridVariables = GetPropType<LowDimTypeTag, Properties::GridVariables>;
+    auto lowDimGridVariables = std::make_shared<LowDimGridVariables>(lowDimProblem, lowDimFvGridGeometry);
+    lowDimGridVariables->init(sol[lowDimIdx]);
+
+    // get some time loop parameters
+    using Scalar = Traits::Scalar;
+    const auto tEnd = getParam<Scalar>("TimeLoop.TEnd");
+    const auto maxDt = getParam<Scalar>("TimeLoop.MaxTimeStepSize");
+    const auto episodeLength = getParam<Scalar>("TimeLoop.EpisodeLength");
+    auto dt = getParam<Scalar>("TimeLoop.DtInitial");
+
+    // intialize the vtk output module
+    using BulkSolutionVector = std::decay_t<decltype(sol[bulkIdx])>;
+    VtkOutputModule<BulkGridVariables, BulkSolutionVector> bulkVtkWriter(*bulkGridVariables, sol[bulkIdx], bulkProblem->name());
+    GetPropType<BulkTypeTag, Properties::IOFields>::initOutputModule(bulkVtkWriter);
+    addElementMarkerForProjection(*couplingManager, bulkVtkWriter);
+    bulkVtkWriter.write(0.0);
+
+    using LowDimSolutionVector = std::decay_t<decltype(sol[lowDimIdx])>;
+    VtkOutputModule<LowDimGridVariables, LowDimSolutionVector> lowDimVtkWriter(*lowDimGridVariables, sol[lowDimIdx], lowDimProblem->name());
+    GetPropType<LowDimTypeTag, Properties::IOFields>::initOutputModule(lowDimVtkWriter);
+    lowDimProblem->addVtkOutputFields(lowDimVtkWriter);
+    lowDimVtkWriter.write(0.0);
+
+    // instantiate time loop
+    auto timeLoop = std::make_shared<CheckPointTimeLoop<Scalar>>(0.0, dt, tEnd);
+    timeLoop->setMaxTimeStepSize(maxDt);
+
+    // the assembler with time loop for instationary problem
+    using Assembler = MultiDomainFVAssembler<Traits, CouplingManager, DiffMethod::numeric>;
+    auto assembler = std::make_shared<Assembler>(std::make_tuple(bulkProblem, lowDimProblem),
+                                                 std::make_tuple(bulkFvGridGeometry, lowDimFvGridGeometry),
+                                                 std::make_tuple(bulkGridVariables, lowDimGridVariables),
+                                                 couplingManager, timeLoop, oldSol);
+
+    // the linear solver
+    using LinearSolver = BlockDiagILU0BiCGSTABSolver;
+    auto linearSolver = std::make_shared<LinearSolver>();
+
+    // the non-linear solver
+    using NewtonSolver = MultiDomainNewtonSolver<Assembler, LinearSolver, CouplingManager>;
+    NewtonSolver nonLinearSolver(assembler, linearSolver, couplingManager);
+
+    // time loop
+    timeLoop->setPeriodicCheckPoint(episodeLength);
+    timeLoop->start();
+    while (!timeLoop->finished())
+    {
+        // solve the non-linear system with time step control
+        nonLinearSolver.solve(sol, *timeLoop);
+
+        // make the new solution the old solution
+        oldSol = sol;
+        bulkGridVariables->advanceTimeStep();
+        lowDimGridVariables->advanceTimeStep();
+
+        // advance to the time loop to the next step
+        timeLoop->advanceTimeStep();
+
+        // output the source terms
+        bulkProblem->computeSourceIntegral(sol[bulkIdx], *bulkGridVariables);
+        lowDimProblem->computeSourceIntegral(sol[lowDimIdx], *lowDimGridVariables);
+
+        // write vtk output
+        if (timeLoop->isCheckPoint() || timeLoop->finished())
+        {
+            bulkVtkWriter.write(timeLoop->time());
+            lowDimVtkWriter.write(timeLoop->time());
+        }
+
+        // report statistics of this time step
+        timeLoop->reportTimeStep();
+
+        // set new dt as suggested by newton controller
+        timeLoop->setTimeStepSize(nonLinearSolver.suggestTimeStepSize(timeLoop->timeStepSize()));
+    }
+
+    timeLoop->finalize(mpiHelper.getCommunication());
+
+    ////////////////////////////////////////////////////////////
+    // finalize, print dumux message to say goodbye
+    ////////////////////////////////////////////////////////////
+
+    // print dumux end message
+    if (mpiHelper.rank() == 0)
+    {
+        Parameters::print();
+        DumuxMessage::print(/*firstCall=*/false);
+    }
+
+    return 0;
+} // end main
---
+++ flow-test-dumux/root_soil/spatialparams_soil.hh
@@ -0,0 +1,112 @@
+// -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set et ts=4 sw=4 sts=4:
+/*****************************************************************************
+ *   See the file COPYING for full copying permissions.                      *
+ *                                                                           *
+ *   This program is free software: you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation, either version 3 of the License, or       *
+ *   (at your option) any later version.                                     *
+ *                                                                           *
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the            *
+ *   GNU General Public License for more details.                            *
+ *                                                                           *
+ *   You should have received a copy of the GNU General Public License       *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ *****************************************************************************/
+/*!
+ * \file
+ * \ingroup EmbeddedTests
+ * \brief Definition of the spatial parameters for the tissue problem.
+ */
+
+#ifndef DUMUX_SOIL_SPATIAL_PARAMS_HH
+#define DUMUX_SOIL_SPATIAL_PARAMS_HH
+
+#include <dumux/common/parameters.hh>
+#include <dumux/porousmediumflow/fvspatialparamsmp.hh>
+#include <dumux/material/fluidmatrixinteractions/2p/vangenuchten.hh>
+
+namespace Dumux {
+
+/*!
+ * \ingroup EmbeddedTests
+ * \brief Definition of the spatial parameters for the tissue problem.
+ */
+template<class GridGeometry, class Scalar>
+class SoilSpatialParams
+: public FVPorousMediumFlowSpatialParamsMP<GridGeometry,Scalar, SoilSpatialParams<GridGeometry, Scalar>>
+{
+    using ThisType = SoilSpatialParams<GridGeometry, Scalar>;
+    using ParentType = FVPorousMediumFlowSpatialParamsMP<GridGeometry, Scalar, ThisType>;
+    using GridView = typename GridGeometry::GridView;
+    using Element = typename GridView::template Codim<0>::Entity;
+    using SubControlVolume = typename GridGeometry::SubControlVolume;
+    using GlobalPosition = typename Element::Geometry::GlobalCoordinate;
+
+    using PcKrSwCurve = FluidMatrix::VanGenuchtenDefault<Scalar>;
+
+public:
+    // export permeability type
+    using PermeabilityType = Scalar;
+
+    SoilSpatialParams(std::shared_ptr<const GridGeometry> gridGeometry)
+    : ParentType(gridGeometry)
+    , pcKrSwCurve_("Soil.SpatialParams")
+    {
+        // perm and poro
+        permeability_ = getParam<Scalar>("Soil.SpatialParams.Permeability");
+        porosity_ = getParam<Scalar>("Soil.SpatialParams.Porosity");
+    }
+
+    /*!
+     * \brief Defines the intrinsic permeability \f$\mathrm{[m^2]}\f$.
+     *
+     * \param element The element
+     * \param scv The sub control volume
+     * \param elemSol The element solution vector
+     */
+    template<class ElementSolution>
+    PermeabilityType permeability(const Element& element,
+                                  const SubControlVolume& scv,
+                                  const ElementSolution& elemSol) const
+    {
+        return permeability_;
+    }
+
+    /*!
+     * \brief Defines the porosity \f$\mathrm{[-]}\f$.
+     *
+     * \param element The current finite element
+     * \param scv The sub control volume
+     * \param elemSol The current element solution vector
+     */
+    template<class ElementSolution>
+    Scalar porosity(const Element& element,
+                    const SubControlVolume& scv,
+                    const ElementSolution& elemSol) const
+    {
+        return porosity_;
+    }
+
+    /*!
+     * \brief Returns the parameters for the material law at a given location
+     *
+     * \param globalPos The global coordinates for the given location
+     */
+    auto fluidMatrixInteractionAtPos(const GlobalPosition& globalPos) const
+    {
+        return makeFluidMatrixInteraction(pcKrSwCurve_);
+    }
+
+private:
+    const PcKrSwCurve pcKrSwCurve_;
+    Scalar permeability_;
+    Scalar porosity_;
+};
+
+} // end namespace Dumux
+
+#endif
